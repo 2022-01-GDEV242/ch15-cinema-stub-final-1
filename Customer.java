3655dda6bcec61291e6dbc9268b16a5f46b6dceb
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2022-05-02 11:11:44 -0400

    cleaned up some code in customer

--- Customer.java
+++ Customer.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;
 /**
  * Write a description of class Customer here.
  *
@@ -8,26 +8,17 @@
 public class Customer
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private Random confermationNumber = new Random(1000000);
+    private double phoneNumber;
+    private double confNumber;
 
     /**
      * Constructor for objects of class Customer
      */
-    public Customer()
+    public Customer(double phoneNumber)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        this.phoneNumber = phoneNumber;
+        confNumber = confermationNumber.nextInt();
+        
     }
 }
