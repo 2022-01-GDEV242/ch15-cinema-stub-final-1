1b831b29ba18ad31575620df40fe241158331487
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2022-05-02 11:45:12 -0400

    new class collections

---
+++ Collections.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+/**
+ * stores all collections we need.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Collections
+{
+    // instance variables - replace the example below with your own
+    String[] titles; 
+    int[] confNums;
+    int[] phoneNums;
+
+    /**
+     * Constructor for objects of class Collections
+     */
+    public Collections()
+    {
+        // initialise instance variables
+        
+    }
+    
+    
+}
